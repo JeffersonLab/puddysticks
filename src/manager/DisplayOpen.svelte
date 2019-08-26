9f8c699c0d428e778f9189cc3ae684acb2ff17cd
ryans <ryans@jlab.org>
2019-08-26 14:44:19 -0400

    version update

--- src/manager/DisplayOpen.svelte
+++ src/manager/DisplayOpen.svelte
@@ -8,13 +8,14 @@
 <div style="text-align: center;">
         <h1>Open Display</h1>
         <h2>Recent</h2>
-        <ul>
-                <li><a href="?display=exampleA.wedm">Example A</a></li>
-                <li><a href="?display=exampleB.wedm">Example B</a></li>
-                <li><a href="?display=exampleC.wedm">Example C</a></li>
-                <li><a href="?display=exampleD.wedm">Example D</a></li>
-        </ul>
         <h2>Local</h2>
         <h2>Remote</h2>
+        <h2>Examples</h2>
+        <ul>
+                <li><a href="?display=examples/exampleA.wedm">Example A</a></li>
+                <li><a href="?display=examples/exampleB.wedm">Example B</a></li>
+                <li><a href="?display=examples/exampleC.wedm">Example C</a></li>
+                <li><a href="?display=examples/exampleD.wedm">Example D</a></li>
+        </ul>
 </div>
 <svelte:options tag="puddy-display-open"/>
