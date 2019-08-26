80e8edcbddd2ef4b29a9a33aea041a9f33d801b7
ryans <ryans@jlab.org>
2019-08-26 16:11:52 -0400

    cleanup

--- src/manager/DisplayOpen.svelte
+++ src/manager/DisplayOpen.svelte
@@ -6,18 +6,18 @@
 <script>
         import LocalFileReader from '../LocalFileReader.svelte';
 </script>
-<div style="text-align: center;">
+<div style="padding: 1em;">
         <h1>Open Display</h1>
-        <h2>Recent</h2>
-        <LocalFileReader on:localfile/>
-        <h2>Local</h2>
         <h2>Remote</h2>
-        <h2>Examples</h2>
+        <h3>Recent</h3>
+        <h3>Examples</h3>
         <ul>
                 <li><a href="?display=examples/exampleA.wedm">Example A</a></li>
                 <li><a href="?display=examples/exampleB.wedm">Example B</a></li>
                 <li><a href="?display=examples/exampleC.wedm">Example C</a></li>
                 <li><a href="?display=examples/exampleD.wedm">Example D</a></li>
         </ul>
+        <h2>Local</h2>
+        <LocalFileReader on:localfile/>
 </div>
 <svelte:options tag="puddy-display-open"/>
