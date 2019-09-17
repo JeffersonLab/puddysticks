4b7b331ab936287dd91f763be4acea9b04571d2d
ryans <ryans@jlab.org>
2019-09-17 16:42:07 -0400

    initial example of epics2web integration

--- src/dataproviders/epics2webGauge.svelte
+++ src/dataproviders/epics2webGauge.svelte
@@ -1,7 +1,21 @@
 <script>
     import Gauge from "../components/Gauge.svelte";
+    import Epics2Web from "../datasources/Epics2Web.svelte";
 
     export let config = {};
+
+    let data = {value: 'No Channel'};
+
+    function update(e) {
+        if(e.detail.value !== undefined) {
+            data.value = e.detail.value;
+        } else if(e.detail.type === 'info') {
+            if(!e.detail.connected) {
+                data.value = 'Disconnected';
+            }
+        }
+    }
 </script>
-<Gauge {config}/>
+<Epics2Web config="{config.dataprovider}" on:value="{update}"/>
+<Gauge {config} {data}/>
 <svelte:options tag="puddy-epics2web-gauge"/>
