f93174bd14948a5a6e07db97b57ad8a8116a6890
ryans <ryans@jlab.org>
2019-09-23 11:19:31 -0400

    Remove Remote and Recent sections for now

--- src/manager/DisplayOpen.svelte
+++ src/manager/DisplayOpen.svelte
@@ -8,12 +8,9 @@
 </script>
 <div style="padding: 1em;">
         <h1>Open Display</h1>
-        <h2>Remote</h2>
-        <h3>Recent</h3>
         <h3>Examples</h3>
         <ul>
                 <li><a href="?display=examples/exampleA.puddy">Example A</a></li>
-                <li><a href="?display=examples/exampleB.puddy">Example B</a></li>
         </ul>
         <h2>Local</h2>
         <LocalFileReader on:localfile/>
